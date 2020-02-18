bcc45e2aed75fee869537cda1ab5ab7ea2271ac7
Christopher Knebel <cknebel@umich.edu>
2020-02-18 12:31:41 -0500

    Made the profile_test script use python3.8

--- examples/profile_test.sh
+++ examples/profile_test.sh
@@ -3,5 +3,5 @@ if [[ $# -ne 1 ]] ;
 	echo "Need exactly one argument: the name of the python script you want to profile"
 	exit 1
 fi
-python -m cProfile -o test_profile.txt $1
-python -c "import pstats; p = pstats.Stats('test_profile.txt'); p.sort_stats('tottime'); p.print_stats()" > test_stats.txt
+python3.8 -m cProfile -o test_profile.txt $1
+python3.8 -c "import pstats; p = pstats.Stats('test_profile.txt'); p.sort_stats('tottime'); p.print_stats()" > test_stats.txt
