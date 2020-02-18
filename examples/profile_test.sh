f822f196834fea287bdbcd690a2f3077ad236c5c
Chris Knebel <cknebel@umich.edu>
2020-02-18 12:24:21 -0500

    Improved script

--- examples/profile_test.sh
+++ examples/profile_test.sh
@@ -1,2 +1,7 @@
+if [[ $# -ne 1 ]] ;
+	then
+	echo "Need exactly one argument: the name of the python script you want to profile"
+	exit 1
+fi
 python -m cProfile -o test_profile.txt $1
-python -c "import pstats; p = pstats.Stats('test_profile.txt'); p.print_stats()" > test_stats.txt
+python -c "import pstats; p = pstats.Stats('test_profile.txt'); p.sort_stats('tottime'); p.print_stats()" > test_stats.txt
