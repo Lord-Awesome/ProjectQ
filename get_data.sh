08a90d744f30369724dadfff53a63e8d180856eb
Christopher Knebel <cknebel@umich.edu>
2020-04-05 15:15:12 -0400

    Script to generate all of the data we need for graphing including iterations

---
+++ get_data.sh
@@ -0,0 +1,25 @@
+NUM_ITER=5
+
+for ((ITER=0; ITER<$NUM_ITER; ITER++));
+do
+	START_TIME=$(date +%s)
+
+	echo $'\n\n---------- Running graph_effect_of_operator_size.sh ----------\n\n'
+	./graph_effect_of_operator_size.sh
+	echo $'\n\n---------- Running graph_effect_of_qubit_ids.sh ----------\n\n'
+	./graph_effect_of_qubit_ids.sh
+	echo $'\n\n---------- Running graph_effect_of_qubit_spacing.sh ----------\n\n'
+	./graph_effect_of_qubit_spacing.sh
+	echo $'\n\n---------- Running graph_effect_of_state_vec_size.sh ----------\n\n'
+	./graph_effect_of_state_vec_size.sh
+
+	cp data/graph_data_operator_size.txt data/iterations/graph_data_operator_size_iter_${ITER}.txt
+	cp data/graph_data_qubitid_magnitude.txt data/iterations/graph_data_qubitid_magnitude_iter_${ITER}.txt
+	cp data/graph_data_qubitid_spacing.txt data/iterations/graph_data_qubitid_spacing_iter_${ITER}.txt
+	cp data/graph_data_state_vec_size.txt data/iterations/graph_data_state_vec_size_iter_${ITER}.txt
+
+	END_TIME=$(date +%s)
+	DURATION=$(($END_TIME - $START_TIME))
+
+	echo $'\n\nIteration ${ITER} is done with time ${DURATION}\n\n'
+done
